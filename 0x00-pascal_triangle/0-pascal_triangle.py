63af97fa9ae5403fdbca2756ceb8dad380121e6d
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-06-30 11:27:25 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-""" Creates a function that returns a lits of lists
-of integers representing the Pascal's triangle of n """
-
 
 def pascal_triangle(n):
-    """ creates a pascal triangle
-    n:
-        number of rows
-    return:
-        Pascal's triangle """
+    
     new_pascal = []
 
     """ Assumes that n is an integer """
